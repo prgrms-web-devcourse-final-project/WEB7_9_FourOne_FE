ebbb1a04d3a2a338d528703615dafd8d37d9af42
Jiseung <ill0o0lli@ajou.ac.kr>
2025-12-21 13:52:01 +0900

    fix: build error

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,17 @@
 import { LoginClient } from '@/components/features/auth/LoginClient'
 import { HomeLayout } from '@/components/layout/HomeLayout'
+import { Suspense } from 'react'
 
 export default function LoginPage() {
   return (
     <HomeLayout>
-      <LoginClient />
+      <Suspense
+        fallback={
+          <div className="flex min-h-screen items-center justify-center"></div>
+        }
+      >
+        <LoginClient />
+      </Suspense>
     </HomeLayout>
   )
 }
